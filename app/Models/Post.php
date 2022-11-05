31035977ec7865cf5cfd85f4c940b88c45fe4879
dy11-laktosa <hady4.1994@gmail.com>
2022-11-05 21:58:26 +0700

    menyelesaikan video 10 menggunakan factory & faker

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,14 +11,16 @@ class Post extends Model
 
     //  protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
-
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
-
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
