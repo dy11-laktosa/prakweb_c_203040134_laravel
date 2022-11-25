fa35d583f4987002ed45d4f06c5fd49fa64c040c
dy11-laktosa <hady4.1994@gmail.com>
2022-11-25 18:20:30 +0700

    Menyelesaikan video N+Problem

--- routes/web.php
+++ routes/web.php
@@ -41,10 +41,9 @@ Route::get('/categories', function () {
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name
+    return view('posts', [
+        'title' => "Post by Category : $category->name",
+        'posts' => $category->posts->load('category', 'author'),
     ]);
 });
 
@@ -52,7 +51,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'title' => 'User Posts',
-        'posts' => $author->posts
+        'title' => "Post by Author : $author->name",
+        'posts' => $author->posts->load('category', 'author'),
     ]);
 });
