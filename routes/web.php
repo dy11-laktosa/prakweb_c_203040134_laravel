0febb1c679169114d61446de71d4f368dc82b094
dy11-laktosa <hady4.1994@gmail.com>
2022-11-27 18:37:08 +0700

    Membuat tampilan login dan registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
+
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,3 +45,8 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
